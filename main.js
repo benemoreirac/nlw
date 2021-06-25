f94334ea50bc49889c66adeb6449ac4289fc8421
Ebenézer Moreira <82347599+benemoreirac@users.noreply.github.com>
2021-06-25 17:25:15 -0300

    Add files via upload
    NLW - Ajustes da Aula05

--- main.js
+++ main.js
@@ -15,9 +15,9 @@ for (const link of links) {
   })
 }
 
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
 function changeHeaderWhenScroll() {
-  const header = document.querySelector('#header')
-  const navHeight = header.offsetHeight
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
@@ -31,7 +31,13 @@ const swiper = new Swiper('.swiper-container', {
     p1: '.swiper-pagination'
   },
   mousewhell: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 
 const scrollReveal = ScrollReveal({
@@ -51,8 +57,8 @@ scrollReveal.reveal(
   { interval: 100 }
 )
 
+const backToTopButton = document.querySelector('.back-to-top')
 function backToTop() {
-  const backToTopButton = document.querySelector('.back-to-top')
   if (window.scrollY >= 500) {
     backToTopButton.classList.add('show')
   } else {
@@ -60,7 +66,31 @@ function backToTop() {
   }
 }
 
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSectio() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+  for (const section of sections) {
+    const sectionTop = section.offSetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint > sectionTop
+    const checkpointEnd = checkpoint < sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
 window.addEventListener('scroll', function () {
   changeHeaderWhenScroll()
   backToTop()
+  activateMenuAtCurrentSectio()
 })
